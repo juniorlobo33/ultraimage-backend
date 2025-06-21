7862795e683e624344b116baa1cb7753272e3e45
juniorlobo33 <134785770+juniorlobo33@users.noreply.github.com>
2025-06-21 02:13:49 -0300

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,1 +1,24 @@
-# main.py backend configurado
+from flask import Flask
+from flask_cors import CORS
+
+from src.routes.user import user_bp
+from src.routes.image import image_bp
+from src.routes.auth import auth_bp
+from src.routes.payment import payment_bp
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+# Rotas
+app.register_blueprint(user_bp, url_prefix='/api')
+app.register_blueprint(image_bp, url_prefix='/api/image')
+app.register_blueprint(auth_bp, url_prefix='/api/auth')
+app.register_blueprint(payment_bp, url_prefix='/api/payment')
+
+# Rota raiz
+@app.route('/')
+def home():
+    return 'API UltraImage rodando com sucesso!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8000)
