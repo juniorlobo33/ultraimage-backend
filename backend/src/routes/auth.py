30926126816c0128c34f7dcc843c9ee1edf3a5ba
juniorlobo33 <134785770+juniorlobo33@users.noreply.github.com>
2025-06-21 15:09:10 -0300

    Update auth.py

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 auth_bp = Blueprint('nome', __name__)
 
-@nome_bp.route('/test-nome')
+@auth_bp.route('/test-nome')
 def test_nome():
     return {'message': 'Rota nome funcionando'}
