dbc9493b487f2688f4faf0b742b189d8d7e0b063
juniorlobo33 <134785770+juniorlobo33@users.noreply.github.com>
2025-06-21 15:09:26 -0300

    Update image.py

--- backend/src/routes/image.py
+++ backend/src/routes/image.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 image_bp = Blueprint('nome', __name__)
 
-@nome_bp.route('/test-nome')
+@image_bp.route('/test-nome')
 def test_nome():
     return {'message': 'Rota nome funcionando'}
