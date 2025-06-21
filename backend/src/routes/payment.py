5dc584937464985fb5c60059416a50dc88401298
juniorlobo33 <134785770+juniorlobo33@users.noreply.github.com>
2025-06-21 15:09:46 -0300

    Update payment.py

--- backend/src/routes/payment.py
+++ backend/src/routes/payment.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 payment_bp = Blueprint('nome', __name__)
 
-@nome_bp.route('/test-nome')
+@payment_bp.route('/test-nome')
 def test_nome():
     return {'message': 'Rota nome funcionando'}
